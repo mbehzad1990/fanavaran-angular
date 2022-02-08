468cf17aca523232c61cdfa26f0adc3113f21375
mbehzad1990 <mehdibehzad@outlook.com>
2022-02-09 00:19:08 +0330

    .

--- src/app/Page/auth/login/login.component.ts
+++ src/app/Page/auth/login/login.component.ts
@@ -33,8 +33,8 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
   formElementInit() {
     this.loginForm = this.fb.group({
-      username: ['admin', Validators.required],
-      password: ['admin1369',
+      username: ['', Validators.required],
+      password: ['',
         Validators.compose([Validators.required, Validators.minLength(8)]),
       ],
       isRemember: [false],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serverUrl:"https://localhost:7071",
+  serverUrl:"https://localhost",
 };
 
 /*
