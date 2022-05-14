a1a4184ea479a790378e7486b7739c42244941a1
mbehzad1990 <mehdibehzad@outlook.com>
2022-05-14 21:27:04 +0430

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl:"http://localhost:5071",
+  serverUrl:"http://185.81.96.33",
   //"http://185.81.96.33",
   //http://localhost:5071/
   version:"2.2.1"
